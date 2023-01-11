2192e40c2364c8b8d15abc89733cbbc106c36d82
Stepan Bryl <bryl.step@tut.by>
2023-01-11 16:19:07 +0300

    init burger menu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.querySelector(".nav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.querySelector(".nav").style.width = "0%";
+    //setTimeout(() => document.querySelector(".nav").style.width = "100%", 10000)
+  }
+
+  window.addEventListener('resize',function(){
+    if(window.innerWidth > 780){
+        document.querySelector(".nav").style.width = "100%";
+    };
+});
